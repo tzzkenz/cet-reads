7b150ff4cdf40a8d1c4fedc0701a77e18ac9be32
tzzkenz <127670903+tzzkenz@users.noreply.github.com>
2025-01-28 18:04:10 +0530

    basic routing

--- cet-reads/src/App.jsx
+++ cet-reads/src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 import SignUp from "./pages/SignUp";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
-    <>
-      <SignUp />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </Router>
   );
 }
 
